435eee257122ab325722a008cf2ac395d58c4008
Adhithya Laxman <adhithyarg26@gmail.com>
2024-01-25 22:02:06 +0530

    Routes added

--- project-root/backend/src/routes/studentRoutes.js
+++ project-root/backend/src/routes/studentRoutes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router()
 
+const StudentController = require('../controllers/studentController')
 
 
+router.post('/api/registration', StudentController.register);
 
-router.post('/')
+
+module.exports = router;
+
+
 
 
 
